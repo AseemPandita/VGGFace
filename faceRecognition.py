15982c6819cd2fcb765cd9485977b6f0cc0bf9d4
Aseem Pandita <aseempandita@live.com>
2021-01-13 14:30:12 +0530

    Added test images and refactored code

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,29 +1,43 @@
 from detection import detect_face
 from vggface import identify_faces
-
+import os
 # Show predicted name if the confidence is more than 80% for the given test image
-THRESHOLD = 0.8
-IMAGEPATH = 'test2.jpg'
+THRESHOLD = 0.6
+
+names = []
+groundTruth = []
 
 
-detected_faces = detect_face(IMAGEPATH).astype('float32')
-identities = identify_faces(detected_faces)
+
+images = os.listdir('images')
+
+for image in images:
+    IMAGEPATH = 'images/'+image
+    detected_faces = detect_face(IMAGEPATH).astype('float32')
+    identities = identify_faces(detected_faces)
 
 
-confidence = 0
-name = 'Unknown'
-for identity in identities[0]:
-    #print(identity[0], identity[1]*100)
-    if identity[1] > THRESHOLD:
-        confidence = identity[1]
-        name = identity[0]
+    confidence = 0
+    name = 'Unknown'
+    for identity in identities[0]:
+        print(identity[0], identity[1]*100)
+        if identity[1] > THRESHOLD:
+            confidence = identity[1]
+            name = identity[0]
 
-print('\n\n\n\n')
+    print('\n\n\n\n')
 
-if name != 'Unknown':
-    name = name.split(' ')[1].split("'")[0]
-    print('Face in the image: {}\nConfidence:{}'.format(name, confidence*100))
-else:
-    print('Could not identify')
+    if name != 'Unknown':
+        name = name.split(' ')[1].split("'")[0]
+        names.append(name)
+        groundTruth.append(image)
+        print('Face in the image: {} [{}] \nConfidence:{}'.format(name, image, confidence*100))
 
-print('\n\n\n\n')
+    else:
+        print('Could not identify; {}'.format(image))
+        names.append('unknown')
+        groundTruth.append(image)
+    print('\n\n\n\n')
+
+print(names)
+print(groundTruth)
